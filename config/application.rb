9187d8dd646c50c5c9851a673cb4c20724de2f80
lytay <lytaytouch@gmail.com>
2015-09-18 07:36:40 +0700

    Add quiet assets gem

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module WebglProject
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.quiet_assets = true
+
   end
 end
