528d2c1a0065f2727ca4954c2e7b359b8ca2d666
lytay <lytaytouch@gmail.com>
2015-09-25 06:23:26 +0700

    Update custome code of application yaml

--- config/application.rb
+++ config/application.rb
@@ -35,11 +35,9 @@ module WebglProject
     #Add Custom of secret.yml
     config.before_configuration do
       env_file = Rails.root.join("config", 'application.yml').to_s
-      if File.exists?(env_file)
-        YAML.load_file(env_file)[Rails.env].each do |key, value|
-          ENV[key.to_s] = value
-        end # end YAML.load_file
-      end # end if File.exists?
+      YAML.load_file(env_file)[Rails.env].each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file) # end YAML.load_file
     end # end config.before_configuration
 
   end
